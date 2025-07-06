7fb1e35a3f1a280c55d4c46e01df2d75876d1ed1
neilesh1987 <neilesh1987@gmail.com>
2025-07-06 19:03:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App2!');
+  res.send('Hello from Dockerized Node.js App234!');
 });
 
 const PORT = process.env.PORT || 3000;
